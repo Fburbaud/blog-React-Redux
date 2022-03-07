650aa3874800de9cb97deb0b72df47b0c783b7f2
Fburbaud <fred.brechet@gmail.com>
2022-03-07 11:05:29 +0100

    list of posts done, fetch users wip

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,13 @@
-export default () => {
-    return 123;
-}; 
+export default (state = [], action) => {
+    /* if(action.type === 'FETCH_POSTS') {
+        return action.payload;
+    } */
+    //in practice we'll see more commonly switch case:
+    switch (action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
+    
+};  
