a2965c1b35b0379fdccc5696866ed015bc5a0eef
Fburbaud <fred.brechet@gmail.com>
2022-03-07 14:11:03 +0100

    that's a wrap

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
-
+import _ from "lodash";
 //with redux-thunk async and await can be used and we can use other action
 // creators with plain objects after this one
 /* export const fetchPosts = () => {
@@ -10,9 +10,39 @@ import jsonPlaceHolder from "../apis/jsonPlaceHolder";
     };
 }; */
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts()); // await for the results before we move oon to an other line of code
+    /* const userIds = _.uniq(_.map(getState().posts, 'userId')); //using lodash to make an array of uniq ids of the array of posts
+    userIds.forEach(id => dispatch(fetchUser(id))); */
+    //just a little refactor:
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();//value to execute all the steps above
+
+};
+
 //refactored with ES2015:
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
-    
-    dispatch({ type: 'FETCH_POSTS', payload: response.data})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data});
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data});
+};
+
+/* Memoize version:
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+};
+//we memoize the function once so it'll call response just once per id
+//with this solution, we can only fetch a user only one time. We can't call
+// fetchUser an other time for something else
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    
+    dispatch({ type: 'FETCH_USER', payload: response.data});
+}); */
